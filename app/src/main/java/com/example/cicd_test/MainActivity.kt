50e497e225c887d6ddd6c13dba0517ba19990152
ltminh <kaito1412.1102@gmail.com>
2024-10-02 10:34:34 +0700

    test

--- app/src/main/java/com/example/cicd_test/MainActivity.kt
+++ app/src/main/java/com/example/cicd_test/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     CICD_TestTheme {
-        Greeting("Android")
+        Greeting("Android CI")
     }
 }
